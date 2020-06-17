fbdfb9a19d6a5dbf5fc7f448cf810238f529645c
unknown <thaiduongx26@gmail.com>
2020-06-18 03:18:57 +0700

    update

--- cdr_trainer.py
+++ cdr_trainer.py
@@ -3,6 +3,7 @@ import torch
 from typing import *
 
 from models.electra_model import ElectraModelClassification
+from transformers import ElectraConfig
 from data_loaders.cdr_dataset import make_cdr_dataset
 from tqdm import tqdm
 from utils.trainer_utils import get_tokenizer
@@ -65,6 +66,8 @@ def train(num_epochs=100):
     test_loader = make_cdr_dataset('data/cdr/CDR_TestSet.PubTator.txt')
 
     tokenizer = get_tokenizer()
+    electra_config = ElectraConfig()
+
     net = ElectraModelClassification.from_pretrained('google/electra-small-discriminator')
     for name, param in net.named_parameters():
         print("name: {}, unfrozen:{}".format(name, param.requires_grad))
@@ -126,8 +129,8 @@ def train(num_epochs=100):
         if do_eval:
             evaluate(net, test_loader, tokenizer)
 
-    # optimizer = torch.optim.Adam([{"params": net.parameters(), "lr": 0.01}])
-    optimizer = optim4GPU(net)
+    optimizer = torch.optim.Adam([{"params": net.parameters(), "lr": 0.01}])
+    # optimizer = optim4GPU(net)
     # scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[2,4,6,8,12,15,18,20,22,24,26,30], gamma=0.8)
     for epoch in range(num_epochs):
         print('Epoch:', epoch)
@@ -137,7 +140,85 @@ def train(num_epochs=100):
             do_eval = True
         train_model(optimizer=optimizer, scheduler=None, tokenizer=tokenizer, do_eval=do_eval)
 
+def pretrain_model(num_epochs=100):
 
+    train_loader = make_cdr_dataset('data/cdr/CDR_TrainingSet.PubTator.txt')
+    test_loader = make_cdr_dataset('data/cdr/CDR_TestSet.PubTator.txt')
+
+    tokenizer = get_tokenizer()
+    electra_config = ElectraConfig()
+    electra_config.vocab_size = tokenizer.vocab_size
+    net = ElectraModelClassification(electra_config)
+    for name, param in net.named_parameters():
+        print("name: {}, unfrozen:{}".format(name, param.requires_grad))
+    if cuda:
+        net.cuda()
+
+    criteria = torch.nn.CrossEntropyLoss(reduction='mean')
+
+    pad_id = tokenizer.pad_token_id
+
+    def train_model(optimizer=None, scheduler=None, tokenizer=None, do_eval=False):
+        net.train()
+        epoch_loss = 0
+        all_labels = []
+        all_preds = []
+        for i, batch in tqdm(enumerate(train_loader)):
+            x, masked_entities_encoded_seqs, chemical_code_seqs, disease_code_seqs, label = batch
+            # print('label = ', label)
+            # label = torch.squeeze(label, 1)
+            attention_mask = (x != pad_id).float()
+            attention_mask = (1. - attention_mask) * -10000.
+            token_type_ids = torch.zeros((x.shape[0], x.shape[1])).long()
+            if cuda:
+                x = x.cuda()
+                label = label.cuda()
+                attention_mask = attention_mask.cuda()
+                token_type_ids = token_type_ids.cuda()
+
+
+            prediction = net(x, token_type_ids=token_type_ids, 
+                                # attention_masks=attention_mask,
+                                  used_entity_token=False, masked_entities_list=masked_entities_encoded_seqs, 
+                                  chemical_code_list=chemical_code_seqs, disease_code_list=disease_code_seqs)
+            
+            loss = criteria(prediction, label)
+            pred = prediction.argmax(dim=-1)
+            all_labels.append(label.data.to('cpu'))
+            all_preds.append(pred.to('cpu'))
+            # print('all_labels: ', all_labels)
+            # print('all_preds: ', all_preds)
+            epoch_loss += loss
+            loss.backward()
+            optimizer.zero_grad()
+            optimizer.step()
+            
+            
+        # scheduler.step()
+            
+        average_loss = epoch_loss / i
+        new_all_labels = []
+        new_all_preds = []
+        for i in range(len(all_labels)):
+            new_all_labels += all_labels[i].tolist()
+            new_all_preds += all_preds[i].tolist()
+
+        from sklearn.metrics import classification_report
+        print("average RE loss : ", average_loss)
+        print("train_cls report: ", classification_report(new_all_labels, new_all_preds))
+        if do_eval:
+            evaluate(net, test_loader, tokenizer)
+
+    optimizer = torch.optim.Adam([{"params": net.parameters(), "lr": 0.01}])
+    # optimizer = optim4GPU(net)
+    # scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[2,4,6,8,12,15,18,20,22,24,26,30], gamma=0.8)
+    for epoch in range(num_epochs):
+        print('Epoch:', epoch)
+        # print('Epoch:', epoch,'LR:', scheduler.get_lr())
+        do_eval = False
+        if epoch % 3 == 0 or epoch == num_epochs - 1:
+            do_eval = True
+        train_model(optimizer=optimizer, scheduler=None, tokenizer=tokenizer, do_eval=do_eval)
 
 if __name__ == '__main__':
     train()
--- data_loaders/cdr_dataset.py
+++ data_loaders/cdr_dataset.py
@@ -146,6 +146,83 @@ class CDR_Sample():
             
         return final_sample, text_tokenized
 
+    def make_example_entity_token(self, use_entity_token: bool = True):
+        from sklearn import preprocessing
+        final_sample = []
+        masked_entities = []
+        text = self.text
+        entities_pos = []
+        for entity_code in self.entities:
+            for pos in self.entities[entity_code]:
+                start = int(pos['start'])
+                end = int(pos['end'])
+                entities_pos.append({
+                    'code': entity_code,
+                    'start': start,
+                    'end': end
+                })
+        entities_pos_sorted = sorted(entities_pos, key=itemgetter('start')) 
+        for pos in reversed(entities_pos_sorted):
+            start = int(pos['start'])
+            end = int(pos['end'])
+            new_text = text[:start] + ' [E] ' + text[start: end] + ' [/E] ' + text[end:]
+            text = new_text
+        
+        e_start_ids = self.tokenize.convert_tokens_to_ids('[e]')
+        e_end_ids = self.tokenize.convert_tokens_to_ids('[/e]')
+
+        text_tokenized = self.tokenize.encode(text)
+        ids = 0
+        entity_check = 0
+        while ids < len(text_tokenized):
+            if text_tokenized[ids] != e_start_ids:
+                masked_entities.append('O')
+                ids += 1
+            else:
+                if use_entity_token:
+                    masked_entities.append(entities_pos_sorted[entity_check]['code'])
+                ids += 1
+                while text_tokenized[ids] != e_end_ids:
+                    masked_entities.append(entities_pos_sorted[entity_check]['code'])
+                    ids += 1
+                if use_entity_token:
+                    masked_entities.append(entities_pos_sorted[entity_check]['code'])
+                entity_check += 1
+                ids += 1
+
+        if not use_entity_token:
+            text_tokenized = list(filter(lambda a: a != e_start_ids and a != e_end_ids, text_tokenized))
+        
+        if len(masked_entities) != len(text_tokenized):
+            print('Ops ! Something wrong, len of masked_entities is {}, while len of text_tokenized is {}'.format(len(masked_entities), len(text_tokenized)))
+        le = preprocessing.LabelEncoder()
+        masked_entities = le.fit_transform(masked_entities)
+        chemical_code_list = self.get_list_code_by_type(type='Chemical')
+        disease_code_list = self.get_list_code_by_type(type='Disease')
+        for chemical_code in chemical_code_list:
+            if chemical_code in ca_entities:
+                final_sample.append({
+                    'text_tokenized': text_tokenized,
+                    'masked_entities': masked_entities,
+                    'chemical_code': le.transform([chemical_code])[0],
+                    'label': 0
+                })
+        for disease_code in disease_code_list:
+            if disease_code in 
+                final_sample.append({
+                    'text_tokenized': text_tokenized,
+                    'masked_entities': masked_entities,
+                    'chemical_code': le.transform([chemical_code])[0],
+                    'disease_code': le.transform([disease_code])[0],
+                    'label': 0
+                })
+        
+        # if len(text_tokenized) > 512:
+        #     subset = masked_entities[511:]
+        #     subset = list(set(subset))
+            
+        return final_sample, text_tokenized
+
 def gen_samples(data_text_raw):
     data = []
     data_cluster = []
