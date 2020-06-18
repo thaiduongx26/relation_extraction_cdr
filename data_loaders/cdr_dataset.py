902a03eaf0af38750a6e8516a031895b93cc7aa0
howard <thaiduongx26@gmail.com>
2020-06-18 12:11:51 +0700

    update

--- data_loaders/cdr_dataset.py
+++ data_loaders/cdr_dataset.py
@@ -146,83 +146,6 @@ class CDR_Sample():
             
         return final_sample, text_tokenized
 
-    def make_example_entity_token(self, use_entity_token: bool = True):
-        from sklearn import preprocessing
-        final_sample = []
-        masked_entities = []
-        text = self.text
-        entities_pos = []
-        for entity_code in self.entities:
-            for pos in self.entities[entity_code]:
-                start = int(pos['start'])
-                end = int(pos['end'])
-                entities_pos.append({
-                    'code': entity_code,
-                    'start': start,
-                    'end': end
-                })
-        entities_pos_sorted = sorted(entities_pos, key=itemgetter('start')) 
-        for pos in reversed(entities_pos_sorted):
-            start = int(pos['start'])
-            end = int(pos['end'])
-            new_text = text[:start] + ' [E] ' + text[start: end] + ' [/E] ' + text[end:]
-            text = new_text
-        
-        e_start_ids = self.tokenize.convert_tokens_to_ids('[e]')
-        e_end_ids = self.tokenize.convert_tokens_to_ids('[/e]')
-
-        text_tokenized = self.tokenize.encode(text)
-        ids = 0
-        entity_check = 0
-        while ids < len(text_tokenized):
-            if text_tokenized[ids] != e_start_ids:
-                masked_entities.append('O')
-                ids += 1
-            else:
-                if use_entity_token:
-                    masked_entities.append(entities_pos_sorted[entity_check]['code'])
-                ids += 1
-                while text_tokenized[ids] != e_end_ids:
-                    masked_entities.append(entities_pos_sorted[entity_check]['code'])
-                    ids += 1
-                if use_entity_token:
-                    masked_entities.append(entities_pos_sorted[entity_check]['code'])
-                entity_check += 1
-                ids += 1
-
-        if not use_entity_token:
-            text_tokenized = list(filter(lambda a: a != e_start_ids and a != e_end_ids, text_tokenized))
-        
-        if len(masked_entities) != len(text_tokenized):
-            print('Ops ! Something wrong, len of masked_entities is {}, while len of text_tokenized is {}'.format(len(masked_entities), len(text_tokenized)))
-        le = preprocessing.LabelEncoder()
-        masked_entities = le.fit_transform(masked_entities)
-        chemical_code_list = self.get_list_code_by_type(type='Chemical')
-        disease_code_list = self.get_list_code_by_type(type='Disease')
-        for chemical_code in chemical_code_list:
-            if chemical_code in ca_entities:
-                final_sample.append({
-                    'text_tokenized': text_tokenized,
-                    'masked_entities': masked_entities,
-                    'chemical_code': le.transform([chemical_code])[0],
-                    'label': 0
-                })
-        for disease_code in disease_code_list:
-            if disease_code in 
-                final_sample.append({
-                    'text_tokenized': text_tokenized,
-                    'masked_entities': masked_entities,
-                    'chemical_code': le.transform([chemical_code])[0],
-                    'disease_code': le.transform([disease_code])[0],
-                    'label': 0
-                })
-        
-        # if len(text_tokenized) > 512:
-        #     subset = masked_entities[511:]
-        #     subset = list(set(subset))
-            
-        return final_sample, text_tokenized
-
 def gen_samples(data_text_raw):
     data = []
     data_cluster = []
@@ -236,6 +159,34 @@ def gen_samples(data_text_raw):
     return data
 
 
+
+def make_cdr_train_dataset(train_path, dev_path, batch_size=16, shuffle=True, num_workers=0):
+    data = []
+    tokenizer = get_tokenizer()
+    count = 0
+    with open(train_path, 'r') as f:
+        raw_data_train = f.readlines()
+    with open(dev_path, 'r') as f:
+        raw_data_dev = f.readlines()
+    data_raw_sample_train = gen_samples(raw_data_train)
+    data_raw_sample_dev = gen_samples(raw_data_dev)
+    for text_block in data_raw_sample_train:
+        sample = CDR_Sample(text_list=text_block, tokenize=tokenizer)
+        count += sample.check_distance_CA()
+        final_sample, text_tokenized = sample.make_example(use_entity_token=False)
+        if len(text_tokenized) <= 512:
+            data += final_sample
+    for text_block in data_raw_sample_dev:
+        sample = CDR_Sample(text_list=text_block, tokenize=tokenizer)
+        count += sample.check_distance_CA()
+        final_sample, text_tokenized = sample.make_example(use_entity_token=False)
+        if len(text_tokenized) <= 512:
+            data += final_sample
+    PS = PadSequenceCDRDataset(token_pad_value=tokenizer.pad_token_id)
+    dataset = CDRDataset(data)
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=0, collate_fn=PS,
+                             pin_memory=False)
+    return data_loader
 
 def make_cdr_dataset(path, batch_size=16, shuffle=True, num_workers=0):
     data = []
