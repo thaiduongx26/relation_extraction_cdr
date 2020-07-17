b0f74adc84e66775efa20cbbe313b329b5534a2b
howard <thaiduongx26@gmail.com>
2020-07-17 17:43:41 +0700

    update dataloader for join label training

--- data_loaders/cdr_dataset.py
+++ data_loaders/cdr_dataset.py
@@ -8,7 +8,7 @@ from operator import itemgetter
 from utils.trainer_utils import get_tokenizer
 from utils.utils import TRAINING_PATH, TESTING_PATH
 from utils.text_utils import check_sentence_contain_entities, extract_sentence_contain_both_2_entities, extract_nearest_sentences_contain_2_entities
-from data_loaders.sequence_padding import PadSequenceCDRDataset, PadSequenceCDRSentenceDataset, PadSequenceNERCDRDataset
+from data_loaders.sequence_padding import PadSequenceCDRDataset, PadSequenceCDRSentenceDataset, PadSequenceNERCDRDataset, PadSequenceCDRFulltextJoinLabelDataset
 
 
 class CDR_Sample():
@@ -371,6 +371,100 @@ class CDR_Sample():
         
         return final_sample
 
+    def make_example_fulltext(self, use_entity_token: bool = True):
+        from sklearn import preprocessing
+        final_sample = []
+        masked_entities = []
+        text = self.text
+        entities_pos = []
+        list_entity_token_idx = []
+        list_entity_token_type = []
+        for entity_code in self.entities:
+            for pos in self.entities[entity_code]:
+                start = int(pos['start'])
+                end = int(pos['end'])
+                entities_pos.append({
+                    'code': entity_code,
+                    'start': start,
+                    'end': end
+                })
+        entities_pos_sorted = sorted(entities_pos, key=itemgetter('start'))
+        for pos in reversed(entities_pos_sorted):
+            start = int(pos['start'])
+            end = int(pos['end'])
+            new_text = text[:start] + ' [E] ' + text[start: end] + ' [/E] ' + text[end:]
+            text = new_text
+        
+        e_start_ids = self.tokenize.convert_tokens_to_ids('[e]')
+        e_end_ids = self.tokenize.convert_tokens_to_ids('[/e]')
+
+        text_tokenized = self.tokenize.encode(text)
+        
+        if len(text_tokenized) > 512:
+            subset = text_tokenized[0:512]
+            e_end_last_idx = [i for i in range(len(subset)) if subset[i] == e_end_ids]
+            end_idx = e_end_last_idx[-1] + 1
+            text_tokenized = text_tokenized[0: end_idx]
+
+        ids = 0
+        entity_check = 0
+        while ids < len(text_tokenized):
+            if text_tokenized[ids] != e_start_ids:
+                masked_entities.append('O')
+                ids += 1
+            else:
+                if use_entity_token:
+                    masked_entities.append(entities_pos_sorted[entity_check]['code'])
+                    list_entity_token_idx.append(ids)
+                    list_entity_token_type.append(self.entities_list[entities_pos_sorted[entity_check]['code']]['entity_type'])
+                ids += 1
+                while text_tokenized[ids] != e_end_ids:
+                    masked_entities.append(entities_pos_sorted[entity_check]['code'])
+                    ids += 1
+                if use_entity_token:
+                    masked_entities.append(entities_pos_sorted[entity_check]['code'])
+                entity_check += 1
+                ids += 1
+
+        if not use_entity_token:
+            text_tokenized = list(filter(lambda a: a != e_start_ids and a != e_end_ids, text_tokenized))
+        if len(masked_entities) != len(text_tokenized):
+            print('Ops ! Something wrong, len of masked_entities is {}, while len of text_tokenized is {}'.format(len(masked_entities), len(text_tokenized)))
+        
+        label_list = []
+        chemical_code_list = self.get_list_code_by_type(type='Chemical')
+        disease_code_list = self.get_list_code_by_type(type='Disease')
+
+        chemical_code_list_recontruct = []
+        disease_code_list_recontruct = []
+        le = preprocessing.LabelEncoder()
+        
+        for chemical_code in chemical_code_list:
+            if chemical_code in masked_entities:
+                chemical_code_list_recontruct.append(chemical_code)
+                for disease_code in disease_code_list:
+                    if disease_code in masked_entities:
+                        if disease_code not in disease_code_list_recontruct:
+                            disease_code_list_recontruct.append(disease_code)
+                        if (chemical_code, disease_code) in self.correct_answers:
+                            label_list.append(1)
+                        else:
+                            label_list.append(0)
+
+        masked_entities = le.fit_transform(masked_entities)
+        chemical_code_list_recontruct = le.transform(chemical_code_list_recontruct)
+        disease_code_list_recontruct = le.transform(disease_code_list_recontruct)
+
+        block = {
+            'text_tokenized': text_tokenized,
+            'masked_entities': masked_entities,
+            'chemical_code_list_encoded': chemical_code_list_recontruct,
+            'disease_code_list_encoded': disease_code_list_recontruct,
+            'label_list': label_list
+        }
+
+        return block
+
 def gen_samples(data_text_raw):
     data = []
     data_cluster = []
@@ -483,6 +577,49 @@ def make_pretrain_ner_dataset(path, use_entity_token=False, batch_size=16, shuff
                              pin_memory=False)
     return data, data_loader
 
+def make_train_joinlabel_dataset(train_path, dev_path, use_entity_token=False, batch_size=16, shuffle=True, num_workers=0):
+    import copy
+    data = []
+    tokenizer = get_tokenizer()
+    with open(train_path, 'r') as f:
+        raw_data_train = f.readlines()
+    with open(dev_path, 'r') as f:
+        raw_data_dev = f.readlines()
+    data_raw_sample_train = gen_samples(raw_data_train)
+    data_raw_sample_dev = gen_samples(raw_data_dev)
+    for text_block in data_raw_sample_train:
+        sample = CDR_Sample(text_list=text_block, tokenize=tokenizer)
+        final_sample = sample.make_example_fulltext(use_entity_token=use_entity_token)
+        data.append(final_sample)
+    for text_block in data_raw_sample_dev:
+        sample = CDR_Sample(text_list=text_block, tokenize=tokenizer)
+        final_sample = sample.make_example_fulltext(use_entity_token=use_entity_token)
+        data.append(final_sample)
+    PS = PadSequenceCDRFulltextJoinLabelDataset(token_pad_value=tokenizer.pad_token_id)
+    dataset = CDRFulltextJoinLabelDataset(data)
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=0, collate_fn=PS,
+                             pin_memory=False)
+    return data, data_loader
+
+
+def make_joinlabel_dataset(path, use_entity_token=False, batch_size=16, shuffle=True, num_workers=0):
+    import copy
+    data = []
+    tokenizer = get_tokenizer()
+    with open(path, 'r') as f:
+        raw_data = f.readlines()
+    data_raw_sample = gen_samples(raw_data)
+    for text_block in data_raw_sample:
+        sample = CDR_Sample(text_list=text_block, tokenize=tokenizer)
+        final_sample = sample.make_example_fulltext(use_entity_token=use_entity_token)
+        data.append(final_sample)
+
+    PS = PadSequenceCDRFulltextJoinLabelDataset(token_pad_value=tokenizer.pad_token_id)
+    dataset = CDRFulltextJoinLabelDataset(data)
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=0, collate_fn=PS,
+                             pin_memory=False)
+    return data, data_loader
+
 class CDRDataset(Dataset):
     def __init__(self, data):
         self.data = data
@@ -525,3 +662,18 @@ class CDRNERDataset(Dataset):
         code_indexes = self.data[idx]['entity_token_idx']
         label = self.data[idx]['label']
         return torch.tensor(text_tokenized), torch.tensor(code_indexes), torch.tensor(label)
+
+class CDRFulltextJoinLabelDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        text_tokenized = self.data[idx]['text_tokenized']
+        masked_entities_encoded = self.data[idx]['masked_entities']
+        chemical_code_list_encoded = self.data[idx]['chemical_code_list_encoded']
+        disease_code_list_encoded = self.data[idx]['disease_code_list_encoded']
+        label_list = self.data[idx]['label_list']
+        return torch.tensor(text_tokenized), torch.tensor(masked_entities_encoded), torch.tensor(chemical_code_list_encoded), torch.tensor(disease_code_list_encoded), torch.tensor(label_list)
--- data_loaders/sequence_padding.py
+++ data_loaders/sequence_padding.py
@@ -60,3 +60,28 @@ class PadSequenceNERCDRDataset():
         code_index = torch.stack(list(map(lambda x: x[1], batch)))
         label = torch.stack(list(map(lambda x: x[2], batch)))
         return seqs, code_index, label
+
+
+class PadSequenceCDRFulltextJoinLabelDataset():
+    """
+    collate_fn for dataloader to collate sequences of different lengths into a fixed length batch
+    Returns padded x sequence, y sequence, x lengths and y lengths of batch
+    """
+
+    def __init__(self, token_pad_value, masked_entities_pad_value=-1, masked_chemical_code_list_encoded=-1, masked_disease_code_list_encoded=-1, masked_label_list=-1):
+        self.token_pad_value = token_pad_value
+        self.masked_entities_pad_value = masked_entities_pad_value
+        
+
+    def __call__(self, batch):
+        seqs = [x[0] for x in batch]
+        masked_entities_encoded_seqs = [x[1] for x in batch]
+        chemical_code_list_encoded_seqs = [x[2] for x in batch]
+        disease_code_list_encoded_seqs = [x[3] for x in batch]
+        label_list_seqs = [x[4] for x in batch]
+        seqs = pad_sequence(seqs, batch_first=True, padding_value=self.token_pad_value)
+        masked_entities_encoded_seqs = pad_sequence(masked_entities_encoded_seqs, batch_first=True, padding_value=self.masked_entities_pad_value)
+        chemical_code_list_encoded_seqs = pad_sequence(chemical_code_list_encoded_seqs, batch_first=True, padding_value=self.masked_chemical_code_list_encoded)
+        disease_code_list_encoded_seqs = pad_sequence(disease_code_list_encoded_seqs, batch_first=True, padding_value=self.masked_disease_code_list_encoded)
+        label_list_seqs = pad_sequence(label_list_seqs, batch_first=True, padding_value=self.masked_label_list)
+        return seqs, masked_entities_encoded_seqs, chemical_code_list_encoded_seqs, disease_code_list_encoded_seqs, label_list_seqs
