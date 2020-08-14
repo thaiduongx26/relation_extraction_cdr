60f34f1a6dde75fbb3bf26559cd4395a36b231de
levi1996 <levi@cinnamon.is>
2020-08-14 01:51:31 -0700

    check len

--- data_loaders/cdr_dataset.py
+++ data_loaders/cdr_dataset.py
@@ -223,6 +223,7 @@ class CDR_Sample():
         text = self.text
         chemical_code_list = self.get_list_code_by_type(type='gene')
         disease_code_list = self.get_list_code_by_type(type='Disease')
+        print("text: ", text)
         print("chemical_code_list: ", chemical_code_list)
         # TODO: FILTER HYPERNYM HERES
         pos_doc_examples = copy.deepcopy(self.correct_answers)
